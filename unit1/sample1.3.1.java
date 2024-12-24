f21c2f092c5e55aaa06512cf13abdc69fa80bec7
水島アキラ <YAMABIKO.yamazusi@gmail.com>
2024-12-24 22:36:12 +0900

    コメント追加

--- unit1/sample1.3.1.java
+++ unit1/sample1.3.1.java
@@ -1,4 +1,5 @@
 public static bool isStringLong(string input)
+// テスト対象となるyコードの総行数は5行
 {
   if(input.length() > 10)
   {
@@ -8,7 +9,10 @@ public static bool isStringLong(string input)
 }
 
 public void Test()
+// テストコードの総行数は4行
 {
   bool result = isStringLong("abc");
   Assert.Eq(result, false);
 }
+
+// よってコード網羅率は4/5 = 80%
