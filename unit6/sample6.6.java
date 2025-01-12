be895d127537fa58de23a6aafbc07ddf8cfd68b4
水島アキラ <YAMABIKO.yamazusi@gmail.com>
2025-01-12 18:26:01 +0900

    sample6.4.java追加

--- unit6/sample6.6.java
+++ unit6/sample6.6.java
@@ -3,16 +3,11 @@
 public void Adding_a_comment_to_an_article()
 {
   var sut = new Article();
-  var text = "This is a test comment";
-  var author = "John Doe";
-  var now  = DateTime.Now;
+  var comment = new Comment("This is a test comment", "John Doe", DateTime.Now);
 
-  sut.AddComment(text, author, now);
+  sut.AddComment(comment);
 
-  // テスト対象のシステムの状態を検証している
-  Assert.Equal(text, sut.Comments[0].Text);
-  Assert.Equal(author, sut.Comments[0].Author);
-  Assert.Equal(now, sut.Comments[0].CreatedAt);
+  sut.Comments.Should().BeEquivalentTo(comment);
 }
 
 // ヘルパーメソッドを使った場合
